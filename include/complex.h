9d7a8e80d6a8aba6d7f491f11972ece400ea0585
Rominos111 <romain.theodet@hotmail.fr>
2020-09-01 00:19:26 +0200

    new: Nombres complexes

---
+++ include/complex.h
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 31/08/2020.
+
+#ifndef FRACTALGENERATOR_COMPLEX_H
+#define FRACTALGENERATOR_COMPLEX_H
+
+#include <stdbool.h>
+#include <math.h>
+
+/**
+ * Nombre complexe
+ */
+typedef struct {
+    double re;
+    double im;
+} Complex;
+
+/**
+ * Clone un complexe
+ *
+ * @param c Complexe à cloner
+ *
+ * @return Copie
+ */
+Complex cloneComplex(Complex c);
+
+/**
+ * Crée un complexe
+ *
+ * @param re Partie réelle
+ * @param im Partie imaginaire
+ *
+ * @return Complexe
+ */
+Complex createComplex(double re, double im);
+
+/**
+ * Complexe égaux ou non
+ *
+ * @param c1 Complexe 1
+ * @param c2 Complexe 2
+ *
+ * @return Égaux ou non
+ */
+bool equalComplex(Complex c1, Complex c2);
+
+/**
+ * Set la partie réelle
+ *
+ * @param c Complexe
+ * @param re Partie réelle
+ */
+void setRe(Complex* c, double re);
+
+/**
+ * Set la partie imaginaire
+ *
+ * @param c Complexe
+ * @param re Partie imaginaire
+ */
+void setIm(Complex* c, double im);
+
+/**
+ * Calcule le module d'un complexe, i.e. <code>sqrt(re<sup>2</sup> + im<sup>2</sup>)</code>
+ *
+ * @param c Complexe
+ *
+ * @return Module
+ */
+double modulusComplex(Complex c);
+
+/**
+ * Somme deux complexes
+ *
+ * @param c1 Complexe 1
+ * @param c2 Complexe 2
+ *
+ * @return Somme des complexes
+ */
+Complex addComplex(Complex c1, Complex c2);
+
+/**
+ * Multiplie deux complexes, i.e. <code>(a + i.b) * (c + i.d) = (a*c - b*d) + i.(a*d + b*c)</code>
+ *
+ * @param c1 Complexe 1
+ * @param c2 Complexe 2
+ *
+ * @return Produit des complexes
+ */
+Complex multComplex(Complex c1, Complex c2);
+
+#endif //FRACTALGENERATOR_COMPLEX_H
---
+++ src/complex.c
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 31/08/2020.
+
+#include "complex.h"
+
+Complex cloneComplex(Complex c) {
+    return (Complex) {
+        .re = c.re,
+        .im = c.im
+    };
+}
+
+Complex createComplex(double re, double im) {
+    return (Complex) {
+        .re = re,
+        .im = im
+    };
+}
+
+bool equalComplex(Complex c1, Complex c2) {
+    return (c1.re == c2.re && c1.im == c2.im);
+}
+
+void setRe(Complex* c, double re) {
+    c->re = re;
+}
+
+void setIm(Complex* c, double im) {
+    c->im = im;
+}
+
+double modulusComplex(Complex c) {
+    return sqrt(c.re*c.re + c.im*c.im);
+}
+
+Complex addComplex(Complex c1, Complex c2) {
+    return createComplex(c1.re + c2.re, c1.im + c2.im);
+}
+
+Complex multComplex(Complex c1, Complex c2) {
+    return createComplex(
+            c1.re * c2.re - c1.im * c2.im,
+            c1.re * c2.im + c1.im * c2.re
+            );
+}
